ed71382b0ecaf27adfcec6895f174fe39368265d
DE-YU <delsin_yu@qq.com>
2024-06-19 23:11:02 +0800

    Add build script

---
+++ Build Mono Editor Win.bat
@@ -0,0 +1,11 @@
+@echo off
+chcp 65001
+
+scons --version
+python --version
+
+set BUILD_NAME=uz
+scons p=windows module_mono_enabled=yes compiledb=yes debug_symbols=yes
+.\bin\godot.windows.editor.x86_64.mono.console.exe --headless --generate-mono-glue modules\mono\glue
+python .\modules\mono\build_scripts\build_assemblies.py --godot-output-dir=.\bin --push-nupkgs-local NugetSource
+pause
