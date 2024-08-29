26442c55988823f0377759dfff355c7def21b368
DE YU <71481700+Delsin-Yu@users.noreply.github.com>
2024-08-29 22:13:40 +0800

    Add team specific description and build scripts

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
-short_name = "godot"
-name = "Godot Engine"
+short_name = "uz_godot"
+name = "Team Dichroic Purplion Build of Godot Engine"
 major = 4
 minor = 3
 patch = 0
-status = "stable"
+status = "customized"
 module_config = ""
 website = "https://godotengine.org"
 docs = "4.3"
