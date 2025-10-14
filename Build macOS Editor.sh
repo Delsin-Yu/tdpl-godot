9a8b00c1e657544ea390ad00817ace642f21c792
DE YU <71481700+Delsin-Yu@users.noreply.github.com>
2025-10-14 21:06:21 +0800

    Fix macOS build scripts

--- Build macOS Editor.sh
+++ Build macOS Editor.sh
@@ -5,5 +5,5 @@ scons p=macos arch=arm64 target=editor profile=custom.py
 lipo -create bin/godot.macos.editor.x86_64.mono bin/godot.macos.editor.arm64.mono -output bin/godot.macos.editor.universal.mono
 rm bin/godot.macos.editor.x86_64.mono
 rm bin/godot.macos.editor.arm64.mono
-bin/godot.macos.editor.universal.mono --headless --generate-mono-glue modules/mono/glue --no-deprecated
-python3 ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir=bin
+bin/godot.macos.editor.universal.mono --headless --generate-mono-glue modules/mono/glue
+python3 ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir=bin --no-deprecated
